64d621fe460a620c3d7a4d0914e229d87aa1baf0
Vismayaks28 <vismayaksofficial@gmail.com>
2026-02-01 05:12:56 +0530

    Backend stable: Resume upload API and admin panel working

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Resume
 # Register your models here.
+
+
+from django.contrib import admin
+from .models import Resume
+
+@admin.register(Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'file', 'uploaded_at')
+    list_filter = ('uploaded_at', 'user')
+    search_fields = ('user__username',)
+
